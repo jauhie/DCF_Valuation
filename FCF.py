ed84d632b32eb1518f8a25dacac80858a49dcefe
kamel <augenkamel@gmail.com>
2025-10-18 14:00:43 +0300

    сценарный анализ

--- FCF.py
+++ FCF.py
@@ -30,20 +30,20 @@ df_fcf['ForecastFCF'] = (df_fcf['EBIT_after_tax'] +
                          df_fcf['CAPEX'] -
                          df_fcf['Delta_WC'])
 
-print("\n=== ПРОГНОЗ FCF ===")
-print(df_fcf)
+#print("\n=== ПРОГНОЗ FCF ===")
+#print(df_fcf)
 
 # Детализированный вывод
-print("\n=== ДЕТАЛИЗАЦИЯ РАСЧЕТА FCF ===")
-for i, row in df_fcf.iterrows():
-    print(f"\n{row['Year']}:")
-    print(f"  EBIT: {row['EBIT']:,.0f}")
-    print(f"  EBIT после налога ({TaxRate_sweden:.1%}): {row['EBIT_after_tax']:,.0f}")
-    print(f"  + Амортизация: {row['Depreciation']:,.0f}")
-    print(f"  - CAPEX: {row['CAPEX']:,.0f}")
-    print(f"  - ΔWorking Capital: {row['Delta_WC']:,.0f}")
-    print(f"  = FCF: {row['ForecastFCF']:,.0f}")
+#print("\n=== ДЕТАЛИЗАЦИЯ РАСЧЕТА FCF ===")
+#for i, row in df_fcf.iterrows():
+#    print(f"\n{row['Year']}:")
+#    print(f"  EBIT: {row['EBIT']:,.0f}")
+#    print(f"  EBIT после налога ({TaxRate_sweden:.1%}): {row['EBIT_after_tax']:,.0f}")
+#    print(f"  + Амортизация: {row['Depreciation']:,.0f}")
+#    print(f"  - CAPEX: {row['CAPEX']:,.0f}")
+#    print(f"  - ΔWorking Capital: {row['Delta_WC']:,.0f}")
+#    print(f"  = FCF: {row['ForecastFCF']:,.0f}")
 
 # Сохранение результатов
 df_fcf.to_csv('fcf_forecast.csv', index=False)
-print(f"\nРезультаты сохранены в 'fcf_forecast.csv'")
+#print(f"\nРезультаты сохранены в 'fcf_forecast.csv'")
--- PV (Present Value).py
+++ PV (Present Value).py
@@ -1,8 +1,31 @@
-
+import numpy as np
+import pandas as pd
+from FCF import df_fcf
+from WACC import WACC_corrected
 
 #---PV = FCF_year / (1 + WACC)^(year - 2024)
 #---Terminal Value = FCF_2034 × (1 + g) / (WACC - g)
 #---PV(Terminal Value) = Terminal Value / (1 + WACC)^5
 
+future_years = np.arange(2025, 2031)
+df_PV = pd.DataFrame({'Year':future_years})
 
+wacc_value = WACC_corrected
+df_PV['FCF'] = df_fcf['ForecastFCF'].values
+df_PV['WACC'] = wacc_value
+df_PV['PV'] = df_PV['FCF'] / ((1+df_PV['WACC'])**(future_years-2024))
+total_pv = df_PV['PV'].sum()
+#print(df_PV['PV'])
 
+g = 0.02
+fcf_2030 = df_PV[df_PV['Year'] == 2030]['FCF'].values[0]
+terminal_value = fcf_2030 * (1 + g) / (WACC_corrected - g)
+pv_terminal = terminal_value / ((1 + WACC_corrected) ** 6)
+
+print(f"Полная стоимость: {total_pv}")
+print(f"Терминальная стоимость: {terminal_value:,.0f}")
+print(f"PV терминальной стоимости: {pv_terminal:,.0f}")
+
+EnterpriceValue = total_pv + pv_terminal
+
+print(f"Стоимость предприятия: {EnterpriceValue:,.0f}")
